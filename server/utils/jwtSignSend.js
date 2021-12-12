c2eb997b1f0cf74c475d74ddc08030b025968c39
avardag <steklofan@gmail.com>
2021-12-12 17:07:51 +0100

    add email activation of account

--- server/utils/jwtSignSend.js
+++ server/utils/jwtSignSend.js
@@ -1,11 +1,5 @@
 const jwt = require("jsonwebtoken");
 const db = require("../db/db");
-/*
-JWT_ACCESS_SECRET=batlgoyab_rag1i_bugo_hab
-JWT_REFRESH_SECRET=kdiofjew09tmflksdmfsg09
-JWT_ACCESS_EXPRIRES_IN=30d
-JWT_REFRESH_EXPRIRES_IN=30d
-JWT_COOKIE_EXPRIRES_IN=30*/
 
 exports.generateTokens = (payload) => {
   const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
@@ -33,50 +27,4 @@ exports.saveTokenToDB = async (userId, refreshToken) => {
     })
     .returning("*");
   return token;
-};
-
-//////////////////////////////
-//old
-
-const signToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPRIRES_IN,
-  });
-};
-
-/**
- *
- * @param {Object} user
- * @param {Number} statusCode
- * @param {Object} res
- */
-exports.createAndSendToken = (user, statusCode, res) => {
-  const token = signToken(user.id);
-
-  //set cookie
-  const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPRIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-    // secure: true, //
-  };
-  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
-
-  res.cookie("jwt_auth", token, cookieOptions);
-  const userDataToSend = {
-    id: user.id,
-    email: user.email,
-    firstName: user.first_name,
-    lastName: user.last_name,
-  };
-
-  //send response
-  res.status(statusCode).json({
-    status: "success",
-    token,
-    data: {
-      user: userDataToSend,
-    },
-  });
 };
