c5bf21e3a0897889e5a41ab1d62203e9bc3dddc2
avardag <steklofan@gmail.com>
2022-06-14 19:20:09 +0200

    email 7

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,54 +1,33 @@
-// const nodemailer = require("nodemailer");
-// exports.sendActivationMail = async (to, link) => {
-//   //1)Create a transporter
-
-//   //Gmail example
-//   const transporter = nodemailer.createTransport({
-//     host: process.env.EMAIL_HOST,
-//     port: process.env.EMAIL_PORT,
-//     secure: false,
-//     requireTLS: true,
-//     auth: {
-//       user: process.env.EMAIL_USERNAME,
-//       pass: process.env.EMAIL_PASSWORD,
-//     },
-//   });
-//   //activate in GMAIL "less secure app" option
-
-//   //2)define email options
-//   const mailOptions = {
-//     // from: `LaundryQ <${process.env.EMAIL_USERNAME}>`,
-//     from: 'Laundry Q" <steklofan@gmail.com>',
-//     to,
-//     subject: `Account activation on ${process.env.API_URL}`,
-//     text: "",
-//     html: `
-//     <div>
-//         <h1>For Account activation, please click the link below</h1>
-//         <a href="${link}">${link}</a>
-//     </div>
-//     `,
-//     headers: { "x-cloudmta-class": "standard" },
-//   };
-//   //3)send email
-//   await transporter.sendMail(mailOptions);
-// };
-
-const sgMail = require("@sendgrid/mail");
-
+const nodemailer = require("nodemailer");
 exports.sendActivationMail = async (to, link) => {
-  sgMail.setApiKey(process.env.EMAIL_PASSWORD);
-  const msg = {
-    to: to, // Change to your recipient
-    from: "steklofan@gmail.com", // Change to your verified sender
+  //1)Create a transporter
+
+  //Gmail example
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false,
+    requireTLS: true,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  //2)define email options
+  const mailOptions = {
+    // from: `LaundryQ <${process.env.EMAIL_USERNAME}>`,
+    from: process.env.EMAIL_FROM,
+    to,
     subject: `Account activation on ${process.env.API_URL}`,
     text: "",
     html: `
-       <div>
-           <h1>For Account activation, please click the link below</h1>   
-           <a href="${link}">${link}</a> 
-       </div>
-       `,
+    <div>
+        <h1>For Account activation, please click the link below</h1>
+        <a href="${link}">${link}</a>
+    </div>
+    `,
   };
-  await sgMail.send(msg);
+  //3)send email
+  await transporter.sendMail(mailOptions);
 };
